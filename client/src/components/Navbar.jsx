c419050e249c352df1b06b7aa50ba35eb1679553
waibaBikash <kavrelibikash@gmail.com>
2026-01-13 22:38:01 +1100

    style: refactor Navbar component for improved readability and formatting

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,11 +15,17 @@ const Navbar = () => {
 
   return (
      <nav className='h-20'>
-                <div className='fixed left-0 top-0 right-0 z-100 flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white transition-all'>
-                    <img onClick={()=> {navigate('/'); scrollTo(0, 0)}} src={assets.logo} alt="logo" className='h-10 cursor-pointer' />
+                <div 
+                className='fixed left-0 top-0 right-0 z-100 flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white transition-all'>
+                    <img 
+                    onClick={()=> {navigate('/'); scrollTo(0, 0)}} src={assets.logo} 
+                    alt="logo" 
+                    className='h-10 cursor-pointer' 
+                    />
 
                     {/* Desktop Menu */}
-                    <div className='hidden sm:flex items-center gap-4 md:gap-8 max-md:text-sm text-gray-800'>
+                    <div 
+                     className='hidden sm:flex items-center gap-4 md:gap-8 max-md:text-sm text-gray-800'>
                         <Link to='/' onClick={() => scrollTo(0, 0)}> Home </Link>
                         <Link to='/marketplace' onClick={() => scrollTo(0, 0)}> Marketplace </Link>
                         <Link to={user ? '/massages' : '#'} onClick={() => user ? scrollTo(0, 0) : openSignIn()}> Massages </Link>
@@ -28,7 +34,8 @@ const Navbar = () => {
                     </div>
 
                     <div>
-                        <button className='max-sm:hidden cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full'>Login</button>
+                        <button 
+                        className='max-sm:hidden cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full'>Login</button>
                         <MenuIcon onClick={() => setMenuOpen(true)} className='sm:hidden' />
                     </div>
 
@@ -42,8 +49,11 @@ const Navbar = () => {
                         <button>My Listing</button>
 
                         
-                        <button className=' cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full'>Login</button>
-                        <XIcon onClick={()=> setMenuOpen(false)} className='absolute size-8 right-6 top-6 text-gray-500 hover:text-gray-700 cursor-pointer'/>
+                        <button 
+                         className=' cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full'>
+                         Login</button>
+                        <XIcon onClick={()=> setMenuOpen(false)}
+                       className='absolute size-8 right-6 top-6 text-gray-500 hover:text-gray-700 cursor-pointer'/>
                     </div>
                 </div>
             </nav>
