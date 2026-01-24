7e7269ff8c26108100f6013f7b3bcbe892a18223
waibaBikash <kavrelibikash@gmail.com>
2026-01-24 23:55:02 +1100

    feat: integrate Redux store and connect LatestListings component to state

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import listingReducer from './features/listingSlice'
 
 export const store = configureStore({
    reducer: {
-
+      listing: listingReducer
    }
 
 })
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
  import { ClerkProvider } from '@clerk/clerk-react'
+import { Provider } from 'react-redux'
+import { store } from './app/Store.js'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -15,7 +17,10 @@ createRoot(document.getElementById('root')).render(
   
     <BrowserRouter>
        <ClerkProvider publishableKey={PUBLISHABLE_KEY} appearance={{variables: {colorPrimary: '#4f46e5'}}}>
-        <App />
+        <Provider store={store}>
+           <App />
+        </Provider>
+        
       </ClerkProvider>
     </BrowserRouter>
     
