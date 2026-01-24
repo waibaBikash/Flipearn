d6d672770d9e16c5871d6da81c5d3ddfb208c670
waibaBikash <kavrelibikash@gmail.com>
2026-01-24 23:39:39 +1100

    feat: add LatestListings and Title components; implement Redux store with listingSlice

---
+++ client/src/components/LatestListings.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from './Title'
+
+const LatestListings = () => {
+  return (
+    <div className='mt-20 mb-8' >
+        <Title title="Latest Listings" description="Discover the hottest social profile available right now." />
+    </div>
+  )
+}
+
+export default LatestListings
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import LatestListings from '../components/LatestListings'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <LatestListings />
     </div>
   )
 }
