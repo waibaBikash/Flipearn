8ff8388e03c0b0eee3f7fb1b7080b7cc185bbd3a
waibaBikash <kavrelibikash@gmail.com>
2026-01-25 21:56:10 +1100

    feat: add ListingCard component and integrate it into LatestListings for improved listing display

--- client/src/components/LatestListings.jsx
+++ client/src/components/LatestListings.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from './Title'
 import { useSelector } from 'react-redux'
+import ListingCard from './ListingCard.jsx';
 
 const LatestListings = () => {
 
@@ -12,8 +13,7 @@ const LatestListings = () => {
           <div className='flex flex-col gap-6 px-6'>  
             {listings.slice(0,4).map((listing, index)=>(
               <div key={index} className='mx-auto w-full max-w-3xl rounded-xl'>
-                   <h1>{listing.title}</h1>
-                   <h1>{listing.username}</h1>
+                   <ListingCard listing={listing} />
               </div>
             ))}
 
---
+++ client/src/components/ListingCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { platformIcons } from '../assets/assets';
+import { BadgeCheck } from 'lucide-react';
+
+const ListingCard = ({listing}) => {
+
+  const currency = import.meta.env.VITE_CURRENCY || '$';
+
+  return (
+    <div className='relative bg-white rounded-2xl shadow-sm border border-gray-100 overflow-hidden hover:shadow-md transition'>
+      {/* Featured Banner */}
+{ listing.featured &&(
+  <>
+    <p className='py-1' />
+    <div className='absolute top-0 left-0 w-full bg-gradient-to-r form-pink-500 to-purple-500 text-white text-center text-xs font-semibold py-1 tracking-wide uppercase'>Featured</div>
+  </>
+)}
+  
+  <div className='p-5 pt-8'> 
+ {/* Header */}
+
+  <div className='flex items-center gap-3 mb-3'>
+   {platformIcons[listing.platform]}
+
+    <div className='flex flex-col'>
+      <h2>{listing.title}</h2>
+      <p>@{listing.username} -  <span className='capitalize'>{listing.platform}</span></p>
+    </div>
+     {listing.verified && <BadgeCheck className='text-green-500 ml-auto w-5 h-5' />}
+  </div>
+  </div>
+    </div>
+  )
+}
+
+export default ListingCard
