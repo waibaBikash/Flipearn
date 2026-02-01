29ffafc4ff79f2f62da0163e91706cb832d30dd9
waibaBikash <kavrelibikash@gmail.com>
2026-02-01 22:39:00 +1100

    feat: add Plans component to Home page

--- client/src/components/Plans.jsx
+++ client/src/components/Plans.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Plans = () => {
   return (
-    <div>
-      
+    <div className='max-w-2xl mx-auto z-20 my-30 max-md:px-4'>
+       <div className='text-center'>
+          <h2 className='text-gray-700 text-4xl font-semibold'>Choose Your Plan</h2>
+          <p className='text-gray-500 text-sm max-w-md mx-auto'>Start for free and scale up as you grow. Find the perfect plan for your content creation needs.
+          </p>
+       </div>
+         <div>
+          
+         </div>
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestListings from '../components/LatestListings'
+import Plans from '../components/Plans'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestListings />
+      <Plans />
     </div>
   )
 }
